f690b88ed857b056cac4f60dae43129a61c03c9e
Igor Federizi <igor.federizi@gmail.com>
2024-09-01 10:20:49 -0300

    creating first test with cypres, user validation

--- cypress/cypress/e2e/users.cy.js
+++ cypress/cypress/e2e/users.cy.js
@@ -1,5 +1,22 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('POST /users',() => {
+  it('Register new User', () => {
+
+    const user = {
+      name: 'Igor Teste',
+      email: 'igor.federizi@gmail.com',
+      password: '123456'
+    }
+
+    cy.request({
+      url: 'users',
+      method: 'POST',
+      body: user,
+      failOnStatusCode: false
+    }).then(response => {
+      expect(response.status).to.eq(200)
+    })
+
+
   })
+
 })
